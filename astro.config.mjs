fa06def3798755bf2efeb900940eb6a305c30404
Rafał Guźniczak <guzniczak.rafal@gmail.com>
2025-08-19 19:33:29 +0200

    Add staging configuration to build in workflow

--- astro.config.mjs
+++ astro.config.mjs
@@ -6,9 +6,12 @@ import { defineConfig } from 'astro/config';
 
 import tailwindcss from '@tailwindcss/vite';
 
+const isStaging = process.env.NODE_ENV === 'staging';
+
 // https://astro.build/config
 export default defineConfig({
-  site: 'https://accessibilitymeetup.pl',
+  site: isStaging ? 'https://accessibility-meetup.github.io' : 'https://accessibilitymeetup.pl',
+  base: isStaging ? '/amp-website' : '',
   integrations: [mdx(), sitemap()],
 
   vite: {
