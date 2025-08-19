3b204ee8c8de32a3f9f1b02c4da8c685e49749ca
Anna Karon <ana.karon@gmail.com>
2025-08-19 20:01:23 +0200

    add amp fonts

--- src/consts.ts
+++ src/consts.ts
@@ -2,4 +2,4 @@
 // You can import this data from anywhere in your site by using the `import` keyword.
 
 export const SITE_TITLE = 'Accessibility Meetup Poznan';
-export const SITE_DESCRIPTION = 'Welcome to Accessibility Meetup Poznan!';
+export const SITE_DESCRIPTION = 'Witamy na stronie Accessibility Meetup Poznan!';
